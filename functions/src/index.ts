cfac6da44c9d42ab2b326bcb486278f2e13403eb
VadimKost <39429087+VadimKost@users.noreply.github.com>
2022-11-20 21:06:22 +0200

    add checking routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,71 @@ import MiniRouter  from './MiniRouter.js';
 
 const miniRouter = new MiniRouter();
 
+// check unimplemented
 miniRouter.get('/unimplemented');
+
+// check 2 similar routes
+miniRouter.get('/helloWorld', (req, res) => {
+	res.send('First');
+});
+
+miniRouter.get('/helloWorld', (req, res) => {
+	res.json({
+		rewriten: true
+	});
+});
+
+// check different methods on single route
+miniRouter.get('/cats', (req, res)=>{
+	res.json({
+		url: req.url,
+		method: 'GET!!!'
+	});
+});
+
+miniRouter.post('/cats', (req, res)=>{
+	res.json({
+		url: req.url,
+		method: 'POST!!!'
+	});
+});
+
+miniRouter.put('/cats', (req, res)=>{
+	res.json({
+		url: req.url,
+		method: 'PUT!!!'
+	});
+});
+
+miniRouter.options('/cats', (req, res)=>{
+	res.json({
+		url: req.url,
+		method: 'OPTIONS!!!'
+	});
+});
+
+// check *
+miniRouter.get('/heroes/*', (req, res) => {
+	res.json({
+		route: '/heroes/*',
+		success: true
+	});
+});
+
+// check :id (params)
+miniRouter.get('/schools/:id', (req, res) => {
+	res.json({
+		route:'/schools/:id',
+		params: req.params,
+	});
+});
+
+miniRouter.get('/schools/:id/hello/:param', (req, res) => {
+	res.json({
+		route:'/schools/:id/hello/:param',
+		params: req.params,
+	});
+});
 
 
 export const router = https.onRequest((req: Request, res: Response) => {
